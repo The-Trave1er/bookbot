30fc8441cabf9a7b93151cf657d4f22e11a97554
Trave1er <harrisondempsey18@gmail.com>
2025-05-25 06:31:10 -0700

    added books

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from stats import word_count, letter_count, sorted_dict
+import sys
+
+book = sys.argv
+
 def get_book_text(file):
 
     with open(file) as bk:
@@ -5,6 +10,19 @@ def get_book_text(file):
 
     return file_contents
 
-frank = get_book_text("books/frankenstein.txt")
+if len(book) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
-print(frank)
+book_text = get_book_text(book[1])
+book_dict = sorted_dict(letter_count(book_text))
+
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {book[1]}...")
+print("----------- Word Count ----------")
+print(f"Found {word_count(book_text)} total words")
+print("--------- Character Count -------")
+for letter in book_dict:
+    if letter["char"].isalpha():
+        print(f"{letter["char"]}: {letter["num"]}")
+print("============= END ===============")
